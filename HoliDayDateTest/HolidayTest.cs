7d43fb5976ca185460d898b484d5d3498e6eccab
Guilherme Lúcio <guilherme.lpcaixeta@gmail.com>
2019-02-13 17:05:36 -0200

    Correction test naming

--- HoliDayDateTest/HolidayTest.cs
+++ HoliDayDateTest/HolidayTest.cs
@@ -11,22 +11,22 @@ namespace Tests
         [TestCase(2019, 1, 21, LocaleHoliday.enUS)]
         [TestCase(2019, 1, 1, LocaleHoliday.ptBr)]
         [TestCase(2019, 1, 1, LocaleHoliday.enUS)]
-        public void TestHolidayTrue(int year, int month, int day, LocaleHoliday locale)
+        public void Test_Date_Holiday_True(int year, int month, int day, LocaleHoliday locale)
         {
-            var falseEnUsHoliday = new DateTime(year, month, day);
-            var result = falseEnUsHoliday.TodayIsAHoliday(locale);
-            Assert.IsTrue(result.IsHoliday);
+            var falseHoliday = new DateTime(year, month, day);
+            var fakeResult = falseHoliday.TodayIsAHoliday(locale);
+            Assert.IsTrue(fakeResult.IsHoliday);
         }
 
         [TestCase(2019, 4, 21, LocaleHoliday.enUS)]
         [TestCase(2019, 1, 21, LocaleHoliday.ptBr)]
         [TestCase(2019, 2, 2, LocaleHoliday.ptBr)]
         [TestCase(2019, 2, 2, LocaleHoliday.enUS)]
-        public void TestHolidayFalse(int year, int month, int day, LocaleHoliday locale)
+        public void Test_Date_Holiday_False(int year, int month, int day, LocaleHoliday locale)
         {
-            var falseEnUsHoliday = new DateTime(year, month, day);
-            var result = falseEnUsHoliday.TodayIsAHoliday(locale);
-            Assert.IsFalse(result.IsHoliday);
+            var falseHoliday = new DateTime(year, month, day);
+            var fakeResult = falseHoliday.TodayIsAHoliday(locale);
+            Assert.IsFalse(fakeResult.IsHoliday);
         }
     }
 }
