f5691faead725d78b0abe091f76581d862275142
Guilherme Lúcio <guilherme.lpcaixeta@gmail.com>
2019-02-20 10:43:58 -0300

    Organize paths repo

--- HoliDayDate/Extension/LocaleHoliday/DictionariesHolidays/DictionaryHolidayPtBr.cs
+++ HoliDayDate/Extension/LocaleHoliday/DictionariesHolidays/DictionaryHolidayPtBr.cs
@@ -13,56 +13,39 @@ namespace HoliDayDate.Locale.Dictionary
             {
                 4, new Dictionary<int, Holiday>()
                 {
-                    {
-                        21, new Holiday("Tiradentes", true)
-                    }
+                    {21, new Holiday("Tiradentes", true)}
                 }
             },
             {
                 5, new Dictionary<int, Holiday>()
                 {
-                    {
-                        1, new Holiday("Dia do Trabalhador", true)
-                    }
+                    {1, new Holiday("Dia do Trabalhador", true)}
                 }
             },
             {
                 6, new Dictionary<int, Holiday>()
                 {
-                    {
-                        24, new Holiday("São João", true)
-                    }
+                    {24, new Holiday("São João", true)}
                 }
             },
             {
                 9, new Dictionary<int, Holiday>()
                 {
-                    {
-                        7, new Holiday("Independência do Brasil", true)
-                    }
+                    {7, new Holiday("Independência do Brasil", true)}
                 }
             },
             {
                 10, new Dictionary<int, Holiday>()
                 {
-                    {
-                        10, new Holiday("Dia das Crianças", true)
-                    }
+                    {10, new Holiday("Dia das Crianças", true)}
                 }
             },
             {
                 11, new Dictionary<int, Holiday>()
                 {
-                    {
-                        2, new Holiday("Dia de Finados", true)
-
-                    },
-                                        {
-                        15, new Holiday("Proclamação da República", true)
-                    },
-                                        {
-                        20, new Holiday("Dia da Consciência Negra", true)
-                    }
+                    {2, new Holiday("Dia de Finados", true)},
+                    {15, new Holiday("Proclamação da República", true)},
+                    {20, new Holiday("Dia da Consciência Negra", true)}
                 }
             },
         };
